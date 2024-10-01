05a60f0104fb8fd187e8e1dafdd7c85d4e5e0492
jungry <seojungrain@gmail.com>
2024-10-01 17:56:46 +0900

    [fea][친구] 유저 목록 출력 API
    - 유저 목록 출력 API 개발

--- src/main/java/com/example/toygry/moneymoa/Friends/controller/FriendsController.java
+++ src/main/java/com/example/toygry/moneymoa/Friends/controller/FriendsController.java
@@ -1,12 +1,12 @@
 package com.example.toygry.moneymoa.Friends.controller;
 
+import com.example.toygry.moneymoa.Friends.dto.UserListResponse;
 import com.example.toygry.moneymoa.Friends.service.FriendsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/friends")
@@ -15,17 +15,18 @@ public class FriendsController {
 
     private final FriendsService friendsService;
 
-    // 1) 사용자 조회 기능
-
+    // 1) 검색을 통한 사용자 조회 기능
     @GetMapping("/users")
-    public ResponseEntity<String> getUserList(
-            @RequestHeader("Authorization") String token
-    ) {
-        return ResponseEntity.ok(friendsService.getUserList(token));
+    public ResponseEntity<List<UserListResponse>> getUserList(
+            @RequestHeader("Authorization") String token,
+            @RequestParam("userId") String id
+    ) throws Exception {
+        return ResponseEntity.ok(friendsService.getUserList(token, id));
     }
     // 2) 친구 신청 보내기 기능
     // 3) 친구 신청 수락 기능
     // 4) 친구 목록 불러오기 기능
+
     // 5) 친구 삭제 기능
     // 6) 친구 상태 (status 수정 기능)
 
--- src/main/java/com/example/toygry/moneymoa/utils/KeycloakUserService.java
+++ src/main/java/com/example/toygry/moneymoa/utils/KeycloakUserService.java
@@ -1,5 +1,10 @@
 package com.example.toygry.moneymoa.utils;
 
+import com.example.toygry.moneymoa.Friends.dto.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,13 +13,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+@Slf4j
 @Component
 public class KeycloakUserService {
 
     @Value("${keycloak.admin-uri}")
     private String KEYCLOAK_ADMIN_URI;
 
-    public String getUserList(String accessToken) {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public List<User> getUserList(String accessToken) throws JsonProcessingException {
         String url = KEYCLOAK_ADMIN_URI + "/users";
 
         RestTemplate restTemplate = new RestTemplate();
@@ -26,6 +36,7 @@ public class KeycloakUserService {
 
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
 
-        return response.getBody();
+        // Convert JSON string to List<User>
+        return objectMapper.readValue(response.getBody(), new TypeReference<>() {});
     }
 }
